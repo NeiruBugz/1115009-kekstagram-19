c6a2c9c6feaa12a61ba4281d50366755245b3606
Nail Badiullin <badiullin.nail@gmail.com>
2020-03-05 14:03:18 +0400

    refactor creating DOM elements

--- js/main.js
+++ js/main.js
@@ -36,6 +36,106 @@ var PICTURES_BLOCK = document.querySelector('.pictures');
 var BIG_PICTURE = document.querySelector('.big-picture');
 var BODY_SELECTOR = document.querySelector('body');
 
+var posts = [];
+var flags = [];
+
+var generateArrayOfFlags = function (length) {
+  var resultFlags = [];
+  for (var i = 0; i < length; i++) {
+    resultFlags.push(false);
+  }
+
+  return resultFlags;
+};
+
+flags = generateArrayOfFlags(MAX_OBJECTS);
+
+var pickRandomNumber = function (min, max) {
+  var index = Math.floor(min + Math.random() * (max + 1 - min));
+  if (flags[index]) {
+    return pickRandomNumber(min, max);
+  } else {
+    flags[index] = true;
+    return index;
+  }
+};
+
+var pickRandomNumberWithRepeat = function (min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
+
+var generateMockObject = function () {
+  return {
+    url: 'photos/' + pickRandomNumber(1, MAX_OBJECTS) + '.jpg',
+    description: 'Балуемся с фотиком',
+    likes: pickRandomNumberWithRepeat(15, 250),
+    comments: [
+      {
+        avatar: 'img/avatar-' + pickRandomNumberWithRepeat(1, 6) + '.svg',
+        name: NAMES[pickRandomNumberWithRepeat(0, 5)],
+        message: COMMENTS[pickRandomNumberWithRepeat(0, 5)]
+      }
+    ],
+  };
+};
+
+var generateMockData = function (dataLength) {
+  var mock = [];
+  for (var i = 0; i < dataLength; i++) {
+    mock.push(generateMockObject());
+  }
+
+  return mock;
+};
+
+var createPhotoPost = function (photoData) {
+  var photoPost = POST_TEMPLATE.cloneNode(true);
+  var postPicture = photoPost.querySelector('.picture__img');
+  postPicture.src = photoData.url;
+  var postLikes = photoPost.querySelector('.picture__likes');
+  postLikes.textContent = photoData.likes;
+  var postComments = photoPost.querySelector('.picture__comments');
+  postComments.textContent = photoData.comments.length;
+
+  return photoPost;
+};
+
+var createPicturesFeed = function (DOMElement, mock) {
+  for (var i = 0; i < mock.length; i++) {
+    DOMElement.appendChild(createPhotoPost(mock[i]));
+  }
+};
+
+var fillBigPicturePost = function (index, mock, DOMElement) {
+  var bigPictureImage = DOMElement.querySelector('.big-picture__img');
+  bigPictureImage.children[0].src = mock[index].url;
+  var likesCount = DOMElement.querySelector('.likes-count');
+  likesCount.textContent = mock[index].likes;
+  var commentsCount = DOMElement.querySelector('.comments-count');
+  commentsCount.textContent = String(mock[index].comments.length);
+  var commentsList = DOMElement.querySelector('.social__comment');
+  for (var i = 0; i < mock[index].comments.length; i++) {
+    var commentAvatar = commentsList.querySelector('.social__picture');
+    commentAvatar.src = mock[index].comments[i].avatar;
+    commentAvatar.alt = mock[index].comments[i].name;
+    var commentContent = commentsList.querySelector('.social__text');
+    commentContent.textContent = mock[index].comments[i].message;
+  }
+
+  var socialCaption = DOMElement.querySelector('.social__caption');
+  socialCaption.textContent = mock[index].description;
+
+  var commentsCountGlobal = DOMElement.querySelector('.social__comment-count');
+  commentsCountGlobal.classList.add('hidden');
+  var commentsLoader = DOMElement.querySelector('.comments-loader');
+  commentsLoader.classList.add('hidden');
+};
+
+posts = generateMockData(MAX_OBJECTS);
+
+createPicturesFeed(PICTURES_BLOCK, posts);
+fillBigPicturePost(0, posts, BIG_PICTURE);
+
 var uploadFile = document.querySelector('#upload-file');
 var cancelUpload = document.querySelector('#upload-cancel');
 var photoEditDialog = document.querySelector('.img-upload__overlay');
@@ -217,107 +317,6 @@ var switchFilter = function (control) {
 for (var k = 0; k < effectsControls.length; k++) {
   switchFilter(effectsControls[k]);
 }
-
-
-var posts = [];
-var flags = [];
-
-var generateArrayOfFlags = function (length) {
-  var resultFlags = [];
-  for (var i = 0; i < length; i++) {
-    resultFlags.push(false);
-  }
-
-  return resultFlags;
-};
-
-flags = generateArrayOfFlags(MAX_OBJECTS);
-
-var pickRandomNumber = function (min, max) {
-  var index = Math.floor(min + Math.random() * (max + 1 - min));
-  if (flags[index]) {
-    return pickRandomNumber(min, max);
-  } else {
-    flags[index] = true;
-    return index;
-  }
-};
-
-var pickRandomNumberWithRepeat = function (min, max) {
-  return Math.floor(min + Math.random() * (max + 1 - min));
-};
-
-var generateMockObject = function () {
-  return {
-    url: 'photos/' + pickRandomNumber(1, MAX_OBJECTS) + '.jpg',
-    description: 'Балуемся с фотиком',
-    likes: pickRandomNumberWithRepeat(15, 250),
-    comments: [
-      {
-        avatar: 'img/avatar-' + pickRandomNumberWithRepeat(1, 6) + '.svg',
-        name: NAMES[pickRandomNumberWithRepeat(0, 5)],
-        message: COMMENTS[pickRandomNumberWithRepeat(0, 5)]
-      }
-    ],
-  };
-};
-
-var generateMockData = function (dataLength) {
-  var mock = [];
-  for (var i = 0; i < dataLength; i++) {
-    mock.push(generateMockObject());
-  }
-
-  return mock;
-};
-
-var createPhotoPost = function (photoData) {
-  var photoPost = POST_TEMPLATE.cloneNode(true);
-  var postPicture = photoPost.querySelector('.picture__img');
-  postPicture.src = photoData.url;
-  var postLikes = photoPost.querySelector('.picture__likes');
-  postLikes.textContent = photoData.likes;
-  var postComments = photoPost.querySelector('.picture__comments');
-  postComments.textContent = photoData.comments.length;
-
-  return photoPost;
-};
-
-var createPicturesFeed = function (DOMElement, mock) {
-  for (var i = 0; i < mock.length; i++) {
-    DOMElement.appendChild(createPhotoPost(mock[i]));
-  }
-};
-
-var fillBigPicturePost = function (index, mock, DOMElement) {
-  var bigPictureImage = DOMElement.querySelector('.big-picture__img');
-  bigPictureImage.children[0].src = mock[index].url;
-  var likesCount = DOMElement.querySelector('.likes-count');
-  likesCount.textContent = mock[index].likes;
-  var commentsCount = DOMElement.querySelector('.comments-count');
-  commentsCount.textContent = String(mock[index].comments.length);
-  var commentsList = DOMElement.querySelector('.social__comment');
-  for (var i = 0; i < mock[index].comments.length; i++) {
-    var commentAvatar = commentsList.querySelector('.social__picture');
-    commentAvatar.src = mock[index].comments[i].avatar;
-    commentAvatar.alt = mock[index].comments[i].name;
-    var commentContent = commentsList.querySelector('.social__text');
-    commentContent.textContent = mock[index].comments[i].message;
-  }
-
-  var socialCaption = DOMElement.querySelector('.social__caption');
-  socialCaption.textContent = mock[index].description;
-
-  var commentsCountGlobal = DOMElement.querySelector('.social__comment-count');
-  commentsCountGlobal.classList.add('hidden');
-  var commentsLoader = DOMElement.querySelector('.comments-loader');
-  commentsLoader.classList.add('hidden');
-};
-
-posts = generateMockData(MAX_OBJECTS);
-
-createPicturesFeed(PICTURES_BLOCK, posts);
-fillBigPicturePost(0, posts, BIG_PICTURE);
 
 // BIG_PICTURE.classList.remove('hidden');
 BODY_SELECTOR.classList.add('modal-open');
